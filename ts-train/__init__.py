e5c956ae141a857ea43afdc0cbcc320e97e2ad3d
Minesh A. Jethva <minesh.1291@gmail.com>
2020-05-30 23:07:33 +0300

    Create __init__.py

---
+++ ts-train/__init__.py
@@ -0,0 +1,26 @@
+__doc__ = """
+ts-train - Time-Series Data Analysis, Manipulation and Machine Learning Modeling Library for Python
+===================================================================================================
+ - Data Preparation
+    - Validation
+    - Cleaning
+        - Missing value imputation
+        - Reindex
+        - Resampling
+    - Normalization
+    - Scaling
+    - Transform
+    - Encoding
+    - Embedding
+ - Feature Extraction
+"""
+
+__author__ = "Minesh A. Jethva"
+__copyright__ = ""
+__credits__ = []
+__license__ = "MIT"
+__version__ = "0.1"
+__date__ = "30 May, 2020"
+__maintainer__ = "Minesh A. Jethva"
+__email__ = "minesh.1291@gmail.com"
+__status__ = "Development Status :: 2 - Pre-Alpha"
