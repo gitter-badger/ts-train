776e3835e386624eea3105da035b800e420f3ecd
Minesh A. Jethva <minesh.1291@gmail.com>
2020-07-01 05:53:01 +0300

    Update deepdata.py

--- ts_train/deepdata.py
+++ ts_train/deepdata.py
@@ -5,13 +5,9 @@ Data transformation for deep learning (:mod:`ts_train.deepdata`)
 .. currentmodule:: ts_train.data
 
 .. autosummary::
-   clean_time_series
-   interpolate_series
-   series2matrix
+   dataframe_to_dataset
    
-.. autofunction:: clean_time_series
-.. autofunction:: interpolate_series
-.. autofunction:: series2matrix
+.. autofunction:: dataframe_to_dataset
 
      
 
@@ -20,5 +16,7 @@ import pandas as pd
 
 
 def dataframe_to_dataset(df: pd.DataFrame):
-    
-    return
+   """
+   
+   """
+   return
