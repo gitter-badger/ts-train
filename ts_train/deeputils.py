28a783428ec0002af6b32dba54021981b8ed24c4
Minesh A. Jethva <minesh.1291@gmail.com>
2020-12-20 13:18:57 +0530

    Update deeputils.py

--- ts_train/deeputils.py
+++ ts_train/deeputils.py
@@ -4,12 +4,16 @@ Deep Learning utilities (:mod:`ts_train.deeputils`)
 .. currentmodule:: ts_train.deeputils
 .. autosummary::
    plot_lcurve
+   add_l1l2_regularizer
    TimedStopping
    
 .. autofunction:: plot_lcurve
+.. autofunction:: add_l1l2_regularizer
 .. autoclass:: TimedStopping
 """
 import time
+import os
+import tempfile
 from typing import List 
 import matplotlib.pyplot as plt
 from tensorflow.keras.callbacks import Callback
@@ -92,3 +96,41 @@ class TimedStopping(Callback):
             self.model.stop_training = True
             if self.verbose:
                 print('Stopping after %s seconds.' % self.seconds)
+
+              
+
+def add_l1l2_regularizer(model, l1=0.0, l2=0.0, reg_attributes=None):
+    """
+    Add L1L2 regularization to the whole model with pre-trained weights.
+    - NOTE: This will save and reload the model. Do not call this function inplace but with
+    
+    :Example:
+        >>> model = add_l1l2_regularizer(model, ...)
+    
+    """
+    if not reg_attributes:
+        reg_attributes = ['kernel_regularizer', 'bias_regularizer',
+                          'beta_regularizer', 'gamma_regularizer']
+    if isinstance(reg_attributes, str):
+        reg_attributes = [reg_attributes]
+
+    regularizer = keras.regularizers.l1_l2(l1=l1, l2=l2)
+
+    for layer in model.layers:
+        for attr in reg_attributes:
+            if hasattr(layer, attr):
+                setattr(layer, attr, regularizer)
+
+    # So far, the regularizers only exist in the model config. We need to
+    # reload the model so that Keras adds them to each layer's losses.
+    model_json = model.to_json()
+
+    # Save the weights before reloading the model.
+    tmp_weights_path = os.path.join(tempfile.gettempdir(), 'tmp_weights.h5')
+    model.save_weights(tmp_weights_path)
+
+    # Reload the model
+    model = keras.models.model_from_json(model_json)
+    model.load_weights(tmp_weights_path, by_name=True)
+
+    return model
